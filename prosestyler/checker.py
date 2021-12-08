4bd39367fc7e7c1a8a22bc1039d56ee7fdeb67d4
neonfuzz <addie.kingsland@gmail.com>
2021-12-07 19:27:25 -0800

    Removing two checks.
    "Frequent words" and "Visualize length" are horribly
    buggy and not well-maintained. They're diluting the
    quality of the tool.

--- prosestyler/checker.py
+++ prosestyler/checker.py
@@ -15,10 +15,8 @@ Functions:
 
 
 from datetime import datetime
-from string import punctuation
 
 import argparse
-import numpy as np
 
 from . import resources
 from .checks.adverbs import Adverbs
@@ -33,9 +31,7 @@ from .checks.proselint import ProseLinter
 from .checks.spelling import Speller
 from .checks.weak import Weak
 from .sentence import Text
-from .tools import colors
 from .tools.extended_argparse import BooleanOptionalAction
-from .tools.helper_functions import now_checking_banner
 from .tools.thesaurus import Thesaurus
 
 
@@ -91,12 +87,6 @@ PARSER.add_argument(
 PARSER.add_argument(
     '--lint', action=BooleanOptionalAction, default=False,
     help='Run Proselint on the text')
-PARSER.add_argument(
-    '--frequent', action=BooleanOptionalAction, default=False,
-    help='Show the most frequently used words')
-PARSER.add_argument(
-    '--vis-length', action=BooleanOptionalAction, default=False,
-    help='Visualize sentence lengths')
 
 
 class TextCheck(Text):
@@ -126,8 +116,6 @@ class TextCheck(Text):
         adverbs - highlight adverbs
         noun_phrases - show clunky noun phrases
         proselint - ask Proselint for advice
-        frequent_words - list the most-used words
-        visualize_length - provide visual cues for sentence length
     """
 
     def __init__(self, string, save_file=None, lang='en_US'):
@@ -217,63 +205,6 @@ class TextCheck(Text):
             ans = input('Sorry, try again: ')
         return ans[0].lower()
 
-    def frequent_words(self, num=10):
-        """
-        Print a list of the most commonly used words.
-
-        Ask user word-per-word if they'd like to view occurances
-        in close proximity.
-        """
-        # TODO: We want the lemmas to include spaces somehow?
-        #       lemmatized tokens?
-        #       The end result should be printed out as proper string
-        #       (currently no spaces nor punctuation)
-        now_checking_banner('frequent words')
-        lemmas = [t.lemma_ for s in self.sentences for t in s.nodes
-                  if not t.is_punct and not t.is_stop]
-
-        distr = zip(*np.unique(lemmas, return_counts=True))
-        distr = [x for x in distr if x[1] > 1]
-        distr.sort(key=lambda x: x[1], reverse=True)
-
-        # Print 'num' most frequent words.
-        for i, j in distr[:num]:
-            print('%s: %.02f%%' % (i, j/len(lemmas)*100))
-        print()
-
-        # Ask if user wants to see words in proximity.
-        print('-----')
-        for word, freq in distr:
-            occurs = np.array([
-                i for i, lem in enumerate(lemmas) if lem == word])
-            dists = occurs[1:] - occurs[:-1]
-            # dist_thresh can be less than 30 if the word occurs a lot.
-            dist_thresh = min(30, int(len(lemmas)/freq/3))
-            to_print = occurs[np.where(dists < dist_thresh)]
-            if to_print.shape[0]:
-                print('-----')
-                yes_no = self._ask_user(word, freq, len(to_print))
-                print('-----')
-                if yes_no == 'y':
-                    for i in to_print:
-                        start = max(0, i-int(dist_thresh/2))
-                        stop = min(i+int(1.5*dist_thresh), len(lemmas))
-                        tokens = lemmas[start:stop]
-                        indices = np.where(tokens == word)[0]
-                        colors.tokenprint(tokens, indices)
-                        input('Enter to continue. ')
-                        print('-----')
-
-    def visualize_length(self, char='X'):
-        """Produce a visualization of sentence length."""
-        now_checking_banner('sentence length')
-        for i, sent in enumerate(self._sentences):
-            if sent == '\n\n':
-                print()
-                continue
-            num = len([x for x in sent if x != ' ' and x not in punctuation])
-            print('{: >6}'.format('(%s)' % (i+1)), char*num)
-
 
 def _reset_args_with_list(args):
     if args.l is not None:
@@ -288,8 +219,6 @@ def _reset_args_with_list(args):
         args.adverbs = 'adverbs' in args.l
         args.noun_phrases = 'noun_phrases' in args.l
         args.lint = 'lint' in args.l
-        args.frequent = 'frequent' in args.l
-        args.vis_length = 'vis_length' in args.l
     return args
 
 
@@ -327,10 +256,6 @@ def check():
         text.noun_phrases()
     if args.lint or args.all:
         text.proselint()
-    if args.frequent or args.all:
-        text.frequent_words()
-    if args.vis_length or args.all:
-        text.visualize_length()
 
     # Final result
     print('\n\n%s' % text.string)
